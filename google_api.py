ecff294a3197a01baf59c6c2125d0a58e8fc40c5
David Link <david.link@tommiecopper.com>
2016-12-16 16:39:17 -0500

    fixing config module

--- google_api.py
+++ google_api.py
@@ -8,7 +8,6 @@ from oauth2client import file
 from oauth2client import tools
 
 from utils import lazyproperty
-import config
 
 class GoogleAPI(object):
     '''Preside over Google API'''
@@ -24,35 +23,6 @@ class GoogleAPI(object):
         '''
         self.name = name
         self.version = version
-
-    @lazyproperty
-    def service(self):
-        '''Authenticate and return service connection
-           Storage object writes credentials to file.
-        '''
-
-        storage = file.Storage('analytics.dat')
-        credentials = storage.get()
-
-        if credentials is None or credentials.invalid:
-            credentials = tools.run_flow(self.flow, storage)
-
-        http = credentials.authorize(http = httplib2.Http())
-        service = discovery.build(self.name, self.version, http=http)
-        return service
-
-    @lazyproperty
-    def flow(self):
-        '''Setup and return Flow Object
-           only needed for first time auth
-        '''
-        client_secrets = config.client_json
-        scope='https://www.googleapis.com/auth/analytics.readonly'
-        flow = client.flow_from_clientsecrets(
-            client_secrets,
-            scope=scope,
-            message=tools.message_if_missing(client_secrets))
-        return flow
 
     def get(self, dimensions, metrics, filters, start_date, end_date):
         '''Call Google API for given parameters
@@ -91,7 +61,7 @@ class GoogleAPI(object):
 
         while more_records:
             results = self.service.data().ga().get(
-                ids='ga:%s' % config.profile_id,
+                ids='ga:%s' % self.config.profile_id,
 
                 metrics=metrics,
                 dimensions=dimensions,
@@ -116,6 +86,48 @@ class GoogleAPI(object):
                 more_records = False
 
         return data
+
+    @lazyproperty
+    def service(self):
+        '''Authenticate and return service connection
+           Storage object writes credentials to file.
+        '''
+
+        storage = file.Storage('analytics.dat')
+        credentials = storage.get()
+
+        if credentials is None or credentials.invalid:
+            credentials = tools.run_flow(self.flow, storage)
+
+        http = credentials.authorize(http = httplib2.Http())
+        service = discovery.build(self.name, self.version, http=http)
+        return service
+
+    @lazyproperty
+    def flow(self):
+        '''Setup and return Flow Object
+           only needed for first time auth
+        '''
+        client_secrets = self.config.client_json
+        scope='https://www.googleapis.com/auth/analytics.readonly'
+        flow = client.flow_from_clientsecrets(
+            client_secrets,
+            scope=scope,
+            message=tools.message_if_missing(client_secrets))
+        return flow
+
+    @lazyproperty
+    def config(self):
+        try:
+            return __import__('config')
+        except ImportError, e:
+            print
+            print "Problem: Can not find config.py!"
+            print "  You need to copy config_template.py to config.py"
+            print "  And then update its values"
+            print
+            import sys
+            sys.exit(1)
 
 if __name__ == '__main__':
     ga = GoogleAPI('analytics', 'v3')
